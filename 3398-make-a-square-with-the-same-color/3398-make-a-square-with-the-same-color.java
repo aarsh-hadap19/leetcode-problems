cd60b1a34954874e1b167e830b59707eaaed7804
Aarsh Hadap <91193673+aarsh-hadap19@users.noreply.github.com>
2025-05-18 15:18:33 +0530

    Time: 0 ms (100%), Space: 42 MB (35.67%) - LeetHub

---
+++ 3398-make-a-square-with-the-same-color/3398-make-a-square-with-the-same-color.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canMakeSquare(char[][] grid) {
+        int n=grid.length-1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int count=1;
+                if(grid[i][j]==grid[i][j+1])count++;
+                if(grid[i][j]==grid[i+1][j+1])count++;
+                if(grid[i][j]==grid[i+1][j])count++;
+                if(count==1||count==3||count==4)return true;
+            }
+        }
+        return false;
+    }
+}
